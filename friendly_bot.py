8fdc4d652a09c4d7e94f3459a4a758dd9e1fab78
massadraza <67168675+massadraza@users.noreply.github.com>
2020-08-12 16:25:09 -0400

    Friendly Bot

--- friendly_bot.py
+++ friendly_bot.py
@@ -1,28 +1,21 @@
-
 # All the following imports for this code.
 import discord 
 import time 
-import asyncio 
-from discord.ext import commands 
+import asyncio
 
 
 # The following information and variables for the functions and events. 
 # id = 738868269343309829
 messages = joined = 0 
-client = commands.Bot(command_prefix = "!") 
 
-
-# Start the bot function
 def read_token():
     with open("token.txt", "r") as f:
         lines = f.readlines()
-        return lines[0].strip()
+        return lines[0].strip() 
 
-token = read_token()
+token = read_token() 
 
 client = discord.Client() 
-
-
 
 # Prints data about the messages sent and users joined every 5 minutes to the data.txt file
 async def update_data(): 
@@ -31,7 +24,7 @@ async def update_data():
 
     while not client.is_closed():  
         try:
-            with open("data.txt", "a") as f:
+            with open("data.txt", "a") as f:   
                 f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n")   
 
             messages = 0 
@@ -64,7 +57,7 @@ async def on_member_join(member):
     joined += 1
     for channel in member.guild.channels:
         if str(channel) == "welcomes":
-            await channel.send(f"""Hey there, {member.mention}. Welcome to the Python Bot Test Server""")  
+            await channel.send.Embed(f"""Hey there, {member.mention}. Welcome to the Python Bot Test Server!""")    
 
 
 
@@ -74,46 +67,56 @@ async def on_message(message):
     global messages 
     messages += 1
 
+    # All the variables for the if statements below.
     id = client.get_guild(738868269343309829) 
     channels = ["commands"] 
     valid_users = ["official_mrbox#0177"]  
-    bad_words = ["stupid", "shutup", "bad"]  
     public_channels = ['general', 'commands', 'welcomes']  
 
-    for word in bad_words:
-        if message.content.count (word) > 0:
-            print(f"""User: {message.author} said a bad word in {message.channel} Swear Word: {message.content}""")  
-            await message.channel.purge(limit=1)  
-
-    if message.content == "!help":
-        embed = discord.Embed(title="Help for Bot.py", description="Useful Commands for Bot.py")       
+    # Runs the !help command
+    if message.content == "!help" and valid_users and channels: 
+        embed = discord.Embed(title="Help for Friendly Bot", description="Useful Commands Below")        
         embed.add_field(name="!hello", value="Greets the user with a friendly hello.") 
-        embed.add_field(name="!users", value="Prints out the number of users in the server.")
+        embed.add_field(name="!members", value="Prints out the number of users in the server.") 
         embed.add_field(name="!clear", value="Clears all messages in a channel.")
         embed.add_field(name="!help", value="Lists all the commands for this Bot") 
         embed.add_field(name="!about", value="Version and more Info")
-        await message.channel.send(content=None, embed=embed)
+        await message.channel.send(content=None, embed=embed) 
 
-    if str(message.channel) in channels and str(message.author) in valid_users:  
+    # Runs the !hello and !members command
+    if str(message.channel) in channels and str(message.author) and valid_users:  
         if message.content.find("!hello") != -1:
-            await message.channel.send("Hi") 
+            await message.channel.send(f"""Hi there, {message.author}, thanks for saying hello and I wish you a wonderful day :smile: """)  
         elif message.content == "!members":
-            await message.channel.send(f"""# of Members: {id.member_count}""") 
+            await message.channel.send(f"""There are, {id.member_count} members in this server!""")   
     else:
         print(f"""User: {message.author} tried to do command {message.content}, in channel {message.channel}""") 
 
-    if str(message.channel) in public_channels and str(message.author) in valid_users:
+    # Runs the !clear command
+    if str(message.channel) in public_channels and str(message.author) and valid_users:
         if message.content.find("!clear") != -1:
             await message.channel.purge(limit=5000)
 
-    if message.content == "!about": 
-        embed = discord.Embed(title="About Bot.py", description="Information about Bot.py") 
-        embed.add_field(name="Version", value="BETA 1.0")
+    # Runs the !about command 
+    if message.content == "!about" and channels:   
+        embed = discord.Embed(title="About Friendly Bot", description="By: @official_mrbox#0177") 
+        embed.add_field(name="Data", value="Data will be printed to the server every 5 minutes about members joined and messages sent.") 
+        embed.add_field(name="Owner Use Only", value="This bot can only be used by the owner official_mrbox#0177, if you would like adminstrator role for this Bot please DM official_mrbox#0177")
+        embed.add_field(name="BotOS", value= "6.1") 
+        embed.add_field(name="Serial Number", value="FJWTHJEJ2304J3")
+        embed.add_field(name="Model Number", value="BOT1") 
         embed.add_field(name="Copyright ©2020", value="Rights for all under the trademark discord.py rewrite") 
+        embed.add_field(name="Server Info", value="Hosted on Heroku.com") 
         await message.channel.send(content=None, embed=embed) 
 
+    # Runs the !update command
+    if message.content == "!update" and channels:
+        embed = discord.Embed(title="Software Updates", description="BotOS is currently up to date!")
+        embed.add_field(name="Current Version", value="6.1") 
+        await message.channel.send(content=None, embed=embed) 
 
+# Runs the bot and data.txt file
 client.loop.create_task(update_data()) 
-client.run(token)  
+client.run(token)   
 
 
